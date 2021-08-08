713bea3cb2bfbae73fa2732887e9b771f46c6a72
Christian Vieira <chrdcv@gmail.com>
2021-08-07 22:20:40 -0300

    Commit 9

--- Rental.java
+++ Rental.java
@@ -4,24 +4,7 @@ public class Rental {
    private int _daysRented;
 
   public double getCharge() {
-      // determine amounts for each line
-      double thisAmount = 0;
-      switch (getMovie().getPriceCode()) {
-        case Movie.REGULAR:
-          thisAmount += 2;
-          if (getDaysRented() > 2)
-            thisAmount += (getDaysRented() - 2) * 1.5;
-          break;
-        case Movie.NEW_RELEASE:
-          thisAmount += getDaysRented() * 3;
-          break;
-        case Movie.CHILDRENS:
-          thisAmount += 1.5;
-          if (getDaysRented() > 3)
-            thisAmount += (getDaysRented() - 3) * 1.5;
-          break;
-      }
-    return thisAmount;
+     return _movie.getCharge(_daysRented);
   }
 
   public int getFrequentRenterPoints() {
@@ -41,6 +24,4 @@ public class Rental {
    public Movie getMovie() {
       return _movie;
    }
-
-
 }
