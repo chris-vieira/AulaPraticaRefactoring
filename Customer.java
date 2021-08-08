faa474b96b0a0c0c1e12ee60a20a5128b1ce09a2
Christian Vieira <chrdcv@gmail.com>
2021-08-07 21:25:34 -0300

    Commit 3

--- Customer.java
+++ Customer.java
@@ -17,22 +17,22 @@ public class Customer {
     return _name;
   }
 
-  private double amountFor(Rental each) {
+  private double amountFor(Rental aRental) {
       // determine amounts for each line
       double thisAmount = 0;
-      switch (each.getMovie().getPriceCode()) {
+      switch (aRental.getMovie().getPriceCode()) {
         case Movie.REGULAR:
           thisAmount += 2;
-          if (each.getDaysRented() > 2)
-            thisAmount += (each.getDaysRented() - 2) * 1.5;
+          if (aRental.getDaysRented() > 2)
+            thisAmount += (aRental.getDaysRented() - 2) * 1.5;
           break;
         case Movie.NEW_RELEASE:
-          thisAmount += each.getDaysRented() * 3;
+          thisAmount += aRental.getDaysRented() * 3;
           break;
         case Movie.CHILDRENS:
           thisAmount += 1.5;
-          if (each.getDaysRented() > 3)
-            thisAmount += (each.getDaysRented() - 3) * 1.5;
+          if (aRental.getDaysRented() > 3)
+            thisAmount += (aRental.getDaysRented() - 3) * 1.5;
           break;
       }
     return thisAmount;
@@ -50,7 +50,7 @@ public class Customer {
 
       // add frequent renter points
       frequentRenterPoints++;
-      
+
       // add bonus for a two day new release rental
       if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1)
         frequentRenterPoints++;
